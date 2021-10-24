8e0f901e3a1e22d1439c8b2c4390c4bc99869ed4
신승원 <ssw900528@gmail.com>
2021-10-24 23:16:38 +0900

    1st push

---
+++ InMaker/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InMaker
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            string s = Clipboard.GetText();
+            string[] ss = s.Split('\n');
+            int sl = ss.Length, i;
+            int cnt = 0;
+            for (i = 0; i < sl; i++)
+            {
+                if(ss[i] != "")
+                {
+                    cnt++;
+                }
+            }
+
+            string res = "('Total " + cnt + " Rows'\n";
+            for (i = 0; i < sl; i++)
+            {
+                if(ss[i] != "")
+                {
+                    res += ",'";
+                    res += ss[i].Trim();
+                    res += "'\n";
+                }                
+            }
+            res += ")";            
+            Clipboard.SetText(res);
+        }
+    }
+}
---
+++ Regexer/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Regexer
+{
+    class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            List<string> ops = new List<string>();
+            int menu = 0;
+            foreach (string fs in Directory.GetFiles(Directory.GetCurrentDirectory()))
+            {
+                if (fs.EndsWith(".txt"))
+                {
+                    Console.WriteLine((++menu) + " : " + Path.GetFileName(fs));
+                    ops.Add(fs);
+                }
+            }
+            Console.WriteLine();
+            
+            while (true)
+            {
+                Console.WriteLine("Input Command ...");
+                string input = Console.ReadLine();
+
+                if (int.TryParse(input, out int num))
+                {
+                    if (num == 0)
+                    {
+                        break;
+                    }
+
+                    if (num > 0 && num <= menu)
+                    {
+                        string command = File.ReadAllText(ops[num - 1]);
+                        string[] commands = command.Split('\n');
+                        Console.WriteLine(commands.Length);
+                        if (commands.Length == 3)
+                        {
+                            string res = commands[0].Trim() + Environment.NewLine;
+                            string s = Clipboard.GetText();
+                            string[] sa = s.Split('\n');
+                            int sc = sa.Length, i, j;
+                            for (i = 0; i < sc; i++)
+                            {
+                                string regex = commands[1].Trim();
+                                string sas = sa[i].Trim();
+                                if (sas.Length > 0)
+                                {
+                                    Console.WriteLine(sas);
+                                    string[] sasa = sas.Split(' ');
+                                    int sasac = sasa.Length;
+
+                                    for (j = 0; j < sasac; j++)
+                                    {
+                                        regex = regex.Replace("{" + j + "}", sasa[j]);
+                                    }
+                                    res += regex + Environment.NewLine;
+                                }
+                            }
+                            res += commands[2].Trim() + Environment.NewLine;
+                            Clipboard.SetText(res);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Wrong Command");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("No Menu in List");
+                    }
+                }
+            }
+        }
+    }
+}
