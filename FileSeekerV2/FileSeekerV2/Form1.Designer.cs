d21e347a9e705fdfd5202738a523b90c448dc59f
신승원 <ssw900528@gmail.com>
2022-06-25 23:35:54 +0900

    123

--- FileSeekerV2/FileSeekerV2/Form1.Designer.cs
+++ FileSeekerV2/FileSeekerV2/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace FileSeekerV2
 {
-    partial class Form1
+    partial class FileSeekerV2
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,127 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.DirectoryColumn = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.textBox1.Location = new System.Drawing.Point(13, 16);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(775, 183);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.BaseKeyEvent);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.DirectoryColumn});
+            this.listView1.FullRowSelect = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(13, 205);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(775, 157);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.BaseKeyEvent);
+            // 
+            // DirectoryColumn
+            // 
+            this.DirectoryColumn.Text = "Directory";
+            this.DirectoryColumn.Width = 700;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(13, 368);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(252, 20);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.BaseKeyEvent);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(271, 368);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(517, 20);
+            this.textBox3.TabIndex = 3;
+            this.textBox3.KeyDown += new System.Windows.Forms.KeyEventHandler(this.BaseKeyEvent);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 395);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(252, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "GO";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.BaseKeyEvent);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(271, 399);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(41, 17);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "Zip";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.BaseKeyEvent);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(318, 399);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(62, 17);
+            this.checkBox2.TabIndex = 6;
+            this.checkBox2.Text = "Archive";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.BaseKeyEvent);
+            // 
+            // FileSeekerV2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(800, 433);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.textBox1);
+            this.MaximizeBox = false;
+            this.Name = "FileSeekerV2";
+            this.Text = "FileSeekerV2";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.BaseKeyEvent);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader DirectoryColumn;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
     }
 }
 
--- FileSeekerV2/FileSeekerV2/Form1.cs
+++ FileSeekerV2/FileSeekerV2/Form1.cs
@@ -1,20 +1,259 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
+using System.IO;
+using System.IO.Compression;
+using System.Reflection;
+using System.Runtime.InteropServices;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Windows.Forms;
+using Helper;
 
 namespace FileSeekerV2
 {
-    public partial class Form1 : Form
+    public partial class FileSeekerV2 : Form
     {
-        public Form1()
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool AllocConsole();
+
+        private delegate void SetControlPropertyThreadSafeDelegate(Control control, string propertyName, object propertyValue);
+
+        static bool Stop = false;
+        static readonly List<string> Targets = new List<string>();
+        static readonly List<string> Results = new List<string>();
+
+        static readonly StringBuilder LogText = new StringBuilder();
+
+        static readonly LogHelper logHelper = new LogHelper("Logs");
+        static readonly ConfigHelper configHelper = new ConfigHelper("Configs.txt");
+
+        public FileSeekerV2()
         {
             InitializeComponent();
+
+            if (configHelper.GetConfig("Console") == "1")
+            {
+                AllocConsole();
+            }
+
+            string preset = File.ReadAllText("Preset.txt");
+            string[] dirs = preset.Split('\n');
+            foreach (string dir in dirs)
+            {
+                if (!dir.StartsWith("'"))
+                {
+                    ListViewItem l = listView1.Items.Add(dir);
+                    if (Directory.Exists(l.Text))
+                    {
+                        l.BackColor = Color.Green;
+                    }
+                    else
+                    {
+                        l.BackColor = Color.Red;
+                    }
+                }
+            }
+        }
+
+        private void BaseKeyEvent(object sender, KeyEventArgs args)
+        {
+            if (args.KeyCode == Keys.Enter)
+            {
+                button1_Click(sender, args);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button1.Text == "GO")
+            {
+                Targets.Clear();
+                Results.Clear();
+                LogText.Clear();
+                textBox1.Text = "";
+                button1.Text = "STOP";
+
+                if (listView1.SelectedItems.Count == 0)
+                {
+                    foreach (ListViewItem l in listView1.Items)
+                    {
+                        if (Directory.Exists(l.Text))
+                        {
+                            Targets.Add(l.Text);
+                        }
+                    }
+                }
+                else
+                {
+                    foreach (ListViewItem l in listView1.SelectedItems)
+                    {
+                        if (Directory.Exists(l.Text))
+                        {
+                            Targets.Add(l.Text);
+                        }
+                    }
+                }
+
+                Thread thread = new Thread(Search);
+                thread.Start();
+            }
+            else if (button1.Text == "STOP")
+            {
+                Stop = true;
+                button1.Text = "...";
+                button1.Enabled = false;
+            }
+        }
+
+        private void Search()
+        {
+            SetControlPropertyThreadSafe(button1, "Text", "STOP");
+            SetControlPropertyThreadSafe(checkBox1, "Enabled", false);
+            SetControlPropertyThreadSafe(checkBox2, "Enabled", false);
+
+            string resDir = @"res_" + DateTime.Now.ToString("yyyyMMddHHmmss");
+            if (checkBox2.Checked)
+            {
+                Directory.CreateDirectory(resDir);
+            }
+
+            foreach (string s in Targets)
+            {
+                try
+                {
+                    CheckDir(new DirectoryInfo(s), resDir, textBox2.Text, textBox3.Text);
+                }
+                catch (Exception e)
+                {
+                    logHelper.WriteText(e.ToString());
+                    Print(e.ToString());
+                }
+            }
+
+            if (Stop)
+            {
+                Stop = false;
+            }
+
+            SetControlPropertyThreadSafe(button1, "Text", "GO");
+            SetControlPropertyThreadSafe(button1, "Enabled", true);
+            SetControlPropertyThreadSafe(checkBox1, "Enabled", true);
+            SetControlPropertyThreadSafe(checkBox2, "Enabled", true);
+
+            StringBuilder result = new StringBuilder();
+            foreach (string s in Results)
+            {
+                result.Append(s + Environment.NewLine);
+            }
+            Print(result.ToString() + Results.Count + " Results");
+        }
+
+        private void CheckDir(DirectoryInfo d, string dir, string th, string ch)
+        {
+            if (Stop)
+            {
+                return;
+            }
+
+            foreach (FileInfo f in d.GetFiles())
+            {
+                if (f.Extension == ".zip" && checkBox1.Checked)
+                {
+                    using (ZipArchive za = ZipFile.OpenRead(f.FullName))
+                    {
+                        foreach (ZipArchiveEntry zae in za.Entries)
+                        {
+                            if (zae.FullName.EndsWith(".txt", StringComparison.OrdinalIgnoreCase)
+                                || zae.FullName.EndsWith(".csv", StringComparison.OrdinalIgnoreCase))
+                            {
+                                //if (!File.Exists(dir + @"\" + zae.Name) && zae.Name.Contains(th))
+                                if (zae.Name.Contains(th))
+                                {
+                                    using (Stream s = zae.Open())
+                                    {
+                                        using (StreamReader sr = new StreamReader(s))
+                                        {
+                                            string content = sr.ReadToEnd();
+                                            if (content.Contains(ch))
+                                            {
+                                                Print("\n----------------------------------------\n"
+                                                    + "File Found From Zip : \n" + zae.Name + "\n From : \n" + f.FullName
+                                                    + "\n----------------------------------------\n");
+                                                Results.Add(zae.FullName);
+                                                if (checkBox2.Checked)
+                                                {
+                                                    zae.ExtractToFile(dir + @"\" + zae.Name);
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                else if (f.Extension == ".txt" || f.Extension == ".csv")
+                {
+                    //if (!File.Exists(dir + @"\" + f.Name) && f.Name.Contains(th))
+                    if (f.Name.Contains(th))
+                    {
+                        string content = File.ReadAllText(f.FullName);
+                        if (content.Contains(ch))
+                        {
+                            Print("\n--------------------------------------------------\n"
+                                + "File Found : \n" + dir + @"\" + f.Name + "\n From : \n" + f.FullName
+                                + "\n--------------------------------------------------\n");
+                            Results.Add(f.FullName);
+                            if (checkBox2.Checked)
+                            {
+                                File.Copy(f.FullName, dir + @"\" + f.Name);
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    if (f.Name.Contains(th))
+                    {
+                        Print("\n--------------------------------------------------\n"
+                            + "File Found : \n" + dir + @"\" + f.Name + "\n From : \n" + f.FullName
+                            + "\n--------------------------------------------------\n");
+                        Results.Add(f.FullName);
+                        if (checkBox2.Checked)
+                        {
+                            File.Copy(f.FullName, dir + @"\" + f.Name);
+                        }
+                    }
+                }
+            }
+
+            foreach (DirectoryInfo dd in d.GetDirectories())
+            {
+                CheckDir(dd, dir, th, ch);
+            }
+        }
+
+        private void Print(string s)
+        {
+            Console.WriteLine(s);
+            LogText.Append(Environment.NewLine + s.Replace("\n", Environment.NewLine));
+            SetControlPropertyThreadSafe(textBox1, "Text", LogText.ToString());
+        }
+
+        public static void SetControlPropertyThreadSafe(Control control, string propertyName, object propertyValue)
+        {
+            if (control.InvokeRequired)
+            {
+                control.Invoke(new SetControlPropertyThreadSafeDelegate(SetControlPropertyThreadSafe)
+                    , new object[] { control, propertyName, propertyValue });
+            }
+            else
+            {
+                control.GetType().InvokeMember(
+                    propertyName, BindingFlags.SetProperty, null, control, new object[] { propertyValue });
+            }
         }
     }
 }
