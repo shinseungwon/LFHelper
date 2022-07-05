0d0d7331b674f9a5ba825b7a4aedac0c8ce78aaf
ssw900528 <ssw900528@gmail.com>
2022-07-05 12:46:58 +0900

    111

--- Tester2/Tester2/Program.cs
+++ Tester2/Tester2/Program.cs
@@ -1,15 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.SqlClient;
-using System.Linq;
-using System.Net;
-using System.Net.Sockets;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
-using System.Security.Principal;
-
-namespace Tester2
+﻿namespace Tester2
 {
     class Program
     {
