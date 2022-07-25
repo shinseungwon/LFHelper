fd021900b11374b739f9bd22d20706bfc982806d
ssw900528 <ssw900528@gmail.com>
2022-07-25 15:35:51 +0900

    12345

---
+++ WebShooterV2/WebShooterV2/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WebShooterV2
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool AllocConsole();
+        public Form1()
+        {
+            AllocConsole();
+            InitializeComponent();
+        }
+    }
+}
