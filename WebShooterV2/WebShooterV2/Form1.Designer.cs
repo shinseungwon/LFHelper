ce958aaa0a3856609ece64ff8be4309cb897576f
ssw900528 <ssw900528@gmail.com>
2022-07-26 16:56:59 +0900

    12345

--- WebShooterV2/WebShooterV2/Form1.Designer.cs
+++ WebShooterV2/WebShooterV2/Form1.Designer.cs
@@ -29,13 +29,231 @@ namespace WebShooterV2
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tbUrl = new System.Windows.Forms.TextBox();
+            this.tbHeader = new System.Windows.Forms.TextBox();
+            this.tbBody = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lvData = new System.Windows.Forms.ListView();
+            this.Line = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Status = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.tbData = new System.Windows.Forms.TextBox();
+            this.bApply = new System.Windows.Forms.Button();
+            this.lvResult = new System.Windows.Forms.ListView();
+            this.Title = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Result = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.tbResultDetail = new System.Windows.Forms.TextBox();
+            this.tbResultLike = new System.Windows.Forms.TextBox();
+            this.bExport = new System.Windows.Forms.Button();
+            this.bLoad = new System.Windows.Forms.Button();
+            this.lvTemplate = new System.Windows.Forms.ListView();
+            this.Template = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // tbUrl
+            // 
+            this.tbUrl.Location = new System.Drawing.Point(432, 12);
+            this.tbUrl.Name = "tbUrl";
+            this.tbUrl.Size = new System.Drawing.Size(442, 21);
+            this.tbUrl.TabIndex = 0;
+            // 
+            // tbHeader
+            // 
+            this.tbHeader.Location = new System.Drawing.Point(432, 39);
+            this.tbHeader.Multiline = true;
+            this.tbHeader.Name = "tbHeader";
+            this.tbHeader.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.tbHeader.Size = new System.Drawing.Size(442, 96);
+            this.tbHeader.TabIndex = 1;
+            // 
+            // tbBody
+            // 
+            this.tbBody.Location = new System.Drawing.Point(432, 141);
+            this.tbBody.Multiline = true;
+            this.tbBody.Name = "tbBody";
+            this.tbBody.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.tbBody.Size = new System.Drawing.Size(442, 288);
+            this.tbBody.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(432, 654);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(442, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Go";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.bGo_Click);
+            // 
+            // lvData
+            // 
+            this.lvData.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Line,
+            this.Status});
+            this.lvData.HideSelection = false;
+            this.lvData.Location = new System.Drawing.Point(12, 12);
+            this.lvData.Name = "lvData";
+            this.lvData.Size = new System.Drawing.Size(414, 417);
+            this.lvData.TabIndex = 4;
+            this.lvData.UseCompatibleStateImageBehavior = false;
+            this.lvData.View = System.Windows.Forms.View.Details;
+            // 
+            // Line
+            // 
+            this.Line.Text = "Line";
+            this.Line.Width = 300;
+            // 
+            // Status
+            // 
+            this.Status.Text = "Status";
+            this.Status.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.Status.Width = 100;
+            // 
+            // tbData
+            // 
+            this.tbData.Location = new System.Drawing.Point(12, 436);
+            this.tbData.Multiline = true;
+            this.tbData.Name = "tbData";
+            this.tbData.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.tbData.Size = new System.Drawing.Size(414, 212);
+            this.tbData.TabIndex = 6;
+            // 
+            // bApply
+            // 
+            this.bApply.Location = new System.Drawing.Point(12, 654);
+            this.bApply.Name = "bApply";
+            this.bApply.Size = new System.Drawing.Size(414, 23);
+            this.bApply.TabIndex = 7;
+            this.bApply.Text = "Apply";
+            this.bApply.UseVisualStyleBackColor = true;
+            this.bApply.Click += new System.EventHandler(this.bApply_Click);
+            // 
+            // lvResult
+            // 
+            this.lvResult.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Title,
+            this.Result});
+            this.lvResult.HideSelection = false;
+            this.lvResult.Location = new System.Drawing.Point(880, 12);
+            this.lvResult.Name = "lvResult";
+            this.lvResult.Size = new System.Drawing.Size(458, 417);
+            this.lvResult.TabIndex = 8;
+            this.lvResult.UseCompatibleStateImageBehavior = false;
+            this.lvResult.View = System.Windows.Forms.View.Details;
+            // 
+            // Title
+            // 
+            this.Title.Text = "Title";
+            this.Title.Width = 200;
+            // 
+            // Result
+            // 
+            this.Result.Text = "Result";
+            this.Result.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.Result.Width = 200;
+            // 
+            // tbResultDetail
+            // 
+            this.tbResultDetail.Location = new System.Drawing.Point(880, 436);
+            this.tbResultDetail.Multiline = true;
+            this.tbResultDetail.Name = "tbResultDetail";
+            this.tbResultDetail.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.tbResultDetail.Size = new System.Drawing.Size(458, 183);
+            this.tbResultDetail.TabIndex = 9;
+            // 
+            // tbResultLike
+            // 
+            this.tbResultLike.Location = new System.Drawing.Point(880, 627);
+            this.tbResultLike.Name = "tbResultLike";
+            this.tbResultLike.Size = new System.Drawing.Size(458, 21);
+            this.tbResultLike.TabIndex = 10;
+            // 
+            // bExport
+            // 
+            this.bExport.Location = new System.Drawing.Point(880, 654);
+            this.bExport.Name = "bExport";
+            this.bExport.Size = new System.Drawing.Size(458, 23);
+            this.bExport.TabIndex = 11;
+            this.bExport.Text = "Export";
+            this.bExport.UseVisualStyleBackColor = true;
+            this.bExport.Click += new System.EventHandler(this.bExport_Click);
+            // 
+            // bLoad
+            // 
+            this.bLoad.Location = new System.Drawing.Point(432, 625);
+            this.bLoad.Name = "bLoad";
+            this.bLoad.Size = new System.Drawing.Size(442, 23);
+            this.bLoad.TabIndex = 12;
+            this.bLoad.Text = "Load";
+            this.bLoad.UseVisualStyleBackColor = true;
+            this.bLoad.Click += new System.EventHandler(this.bLoad_Click);
+            // 
+            // lvTemplate
+            // 
+            this.lvTemplate.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Template});
+            this.lvTemplate.HideSelection = false;
+            this.lvTemplate.Location = new System.Drawing.Point(432, 436);
+            this.lvTemplate.Name = "lvTemplate";
+            this.lvTemplate.Size = new System.Drawing.Size(442, 183);
+            this.lvTemplate.TabIndex = 13;
+            this.lvTemplate.UseCompatibleStateImageBehavior = false;
+            this.lvTemplate.View = System.Windows.Forms.View.Details;
+            this.lvTemplate.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.lvTemplate_MouseDoubleClick);
+            // 
+            // Template
+            // 
+            this.Template.Text = "TemplateName";
+            this.Template.Width = 400;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1350, 689);
+            this.Controls.Add(this.lvTemplate);
+            this.Controls.Add(this.bLoad);
+            this.Controls.Add(this.bExport);
+            this.Controls.Add(this.tbResultLike);
+            this.Controls.Add(this.tbResultDetail);
+            this.Controls.Add(this.lvResult);
+            this.Controls.Add(this.bApply);
+            this.Controls.Add(this.tbData);
+            this.Controls.Add(this.lvData);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tbBody);
+            this.Controls.Add(this.tbHeader);
+            this.Controls.Add(this.tbUrl);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "WebShooterV2";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox tbUrl;
+        private System.Windows.Forms.TextBox tbHeader;
+        private System.Windows.Forms.TextBox tbBody;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView lvData;
+        private System.Windows.Forms.ColumnHeader Line;
+        private System.Windows.Forms.ColumnHeader Status;
+        private System.Windows.Forms.TextBox tbData;
+        private System.Windows.Forms.Button bApply;
+        private System.Windows.Forms.ListView lvResult;
+        private System.Windows.Forms.ColumnHeader Title;
+        private System.Windows.Forms.ColumnHeader Result;
+        private System.Windows.Forms.TextBox tbResultDetail;
+        private System.Windows.Forms.TextBox tbResultLike;
+        private System.Windows.Forms.Button bExport;
+        private System.Windows.Forms.Button bLoad;
+        private System.Windows.Forms.ListView lvTemplate;
+        private System.Windows.Forms.ColumnHeader Template;
     }
 }
 
--- WebShooterV2/WebShooterV2/Form1.cs
+++ WebShooterV2/WebShooterV2/Form1.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Helper;
 
 namespace WebShooterV2
 {
@@ -16,10 +19,129 @@ namespace WebShooterV2
         [DllImport("kernel32.dll", SetLastError = true)]
         [return: MarshalAs(UnmanagedType.Bool)]
         static extern bool AllocConsole();
+
+        [DllImport("user32.dll", EntryPoint = "SetWindowPos")]
+        public static extern IntPtr SetWindowPos(IntPtr hWnd, int hWndInsertAfter, int x, int Y, int cx, int cy, int wFlags);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern IntPtr GetConsoleWindow();
+
+        public static readonly List<string> tNames = new List<string>();
+        public static readonly List<string> tContents = new List<string>();
+
+        public static readonly List<string> tUrl = new List<string>();
+        public static readonly List<string> tHeader = new List<string>();
+        public static readonly List<string> tBody = new List<string>();
+
         public Form1()
         {
             AllocConsole();
             InitializeComponent();
+            SetWindowPos(GetConsoleWindow(), 0, -3000, 0, 0, 0, 0x0001);
+            LoadTemplates();
+        }
+
+        private void LoadTemplates()
+        {
+            string dir = Directory.GetCurrentDirectory() + @"\templates\";
+            DirectoryInfo di = new DirectoryInfo(dir);
+
+            tNames.Clear();
+            tContents.Clear();
+
+            tUrl.Clear();
+            tHeader.Clear();
+            tBody.Clear();
+
+            foreach (FileInfo fi in di.GetFiles())
+            {
+                tNames.Add(fi.Name);
+                string content = File.ReadAllText(fi.FullName);
+                tContents.Add(content);
+
+                string[] parts = content.Split(new string[] { "\r\n\r\n" }
+                , StringSplitOptions.RemoveEmptyEntries);
+
+                tUrl.Add(parts[0]);
+                tHeader.Add(parts[1]);
+                tBody.Add(parts[2]);
+            }
+
+            foreach (string s in tNames)
+            {
+                lvTemplate.Items.Add(new ListViewItem(s.Split('.')[0]));
+            }
+        }
+
+        static string WebCommunication(string url, string header, string content)
+        {
+            //Create Request Format
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
+            req.Method = "POST";
+            req.UserAgent = "LFL Jeffrey";
+            req.ContentLength = content.Length;
+
+            string[] headerLines = header.Split(new string[] { "\r\n" }
+                , StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (string s in headerLines)
+            {
+                string[] headerLine = s.Split(new string[] { " " }
+            , StringSplitOptions.RemoveEmptyEntries);
+
+                req.Headers[headerLine[0]] = headerLine[1];
+            }
+
+            using (Stream stream = req.GetRequestStream())
+            using (StreamWriter sw = new StreamWriter(stream))
+            {
+                sw.Write(content);
+            }
+
+            //Get Response
+            HttpWebResponse res = (HttpWebResponse)req.GetResponse();
+            string result = "";
+
+            using (Stream stream = res.GetResponseStream())
+            using (StreamReader sr = new StreamReader(stream))
+            {
+                result = sr.ReadToEnd();
+            }
+
+            return result;
+        }
+
+        private void bApply_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bGo_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bExport_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bLoad_Click(object sender, EventArgs e)
+        {
+            LoadTemplates();
+        }
+
+        private void lvTemplate_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (lvTemplate.SelectedItems.Count == 1)
+            {
+                string target = lvTemplate.SelectedItems[0].Text;
+                int index = lvTemplate.SelectedItems[0].Index;
+
+                tbUrl.Text = tUrl[index];
+                tbHeader.Text = tHeader[index];
+                tbBody.Text = tBody[index];
+            }
         }
     }
 }
