97a6cd06706f786e705608d00ab58d69cde4e95d
ssw900528 <ssw900528@gmail.com>
2022-08-08 16:42:53 +0900

    Pull Complete

--- HyundaiMan/HyundaiMan/Form1.Designer.cs
+++ HyundaiMan/HyundaiMan/Form1.Designer.cs
@@ -30,21 +30,124 @@ namespace HyundaiMan
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.cbCustomer = new System.Windows.Forms.ComboBox();
+            this.cbType = new System.Windows.Forms.ComboBox();
+            this.tbInput = new System.Windows.Forms.TextBox();
+            this.lvResult = new System.Windows.Forms.ListView();
+            this.bSearch = new System.Windows.Forms.Button();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.SuspendLayout();
+            // 
+            // cbCustomer
+            // 
+            this.cbCustomer.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbCustomer.FormattingEnabled = true;
+            this.cbCustomer.Location = new System.Drawing.Point(12, 7);
+            this.cbCustomer.Name = "cbCustomer";
+            this.cbCustomer.Size = new System.Drawing.Size(121, 20);
+            this.cbCustomer.TabIndex = 0;
+            this.cbCustomer.SelectedIndexChanged += new System.EventHandler(this.cbCustomer_SelectedIndexChanged);
+            // 
+            // cbType
+            // 
+            this.cbType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbType.FormattingEnabled = true;
+            this.cbType.Items.AddRange(new object[] {
+            "Order",
+            "Return"});
+            this.cbType.Location = new System.Drawing.Point(139, 7);
+            this.cbType.Name = "cbType";
+            this.cbType.Size = new System.Drawing.Size(121, 20);
+            this.cbType.TabIndex = 2;
+            this.cbType.SelectedIndexChanged += new System.EventHandler(this.cbType_SelectedIndexChanged);
+            // 
+            // tbInput
+            // 
+            this.tbInput.Location = new System.Drawing.Point(12, 33);
+            this.tbInput.Multiline = true;
+            this.tbInput.Name = "tbInput";
+            this.tbInput.Size = new System.Drawing.Size(248, 605);
+            this.tbInput.TabIndex = 3;
+            // 
+            // lvResult
+            // 
+            this.lvResult.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.lvResult.FullRowSelect = true;
+            this.lvResult.HideSelection = false;
+            this.lvResult.Location = new System.Drawing.Point(266, 7);
+            this.lvResult.Name = "lvResult";
+            this.lvResult.Size = new System.Drawing.Size(857, 631);
+            this.lvResult.TabIndex = 4;
+            this.lvResult.UseCompatibleStateImageBehavior = false;
+            this.lvResult.View = System.Windows.Forms.View.Details;
+            // 
+            // bSearch
+            // 
+            this.bSearch.Location = new System.Drawing.Point(1129, 487);
+            this.bSearch.Name = "bSearch";
+            this.bSearch.Size = new System.Drawing.Size(193, 151);
+            this.bSearch.TabIndex = 5;
+            this.bSearch.Text = "Search";
+            this.bSearch.UseVisualStyleBackColor = true;
+            this.bSearch.Click += new System.EventHandler(this.bSearch_Click);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Order#";
+            this.columnHeader1.Width = 180;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Line#";
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Sku#";
+            this.columnHeader3.Width = 180;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "status";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1334, 650);
+            this.Controls.Add(this.bSearch);
+            this.Controls.Add(this.lvResult);
+            this.Controls.Add(this.tbInput);
+            this.Controls.Add(this.cbType);
+            this.Controls.Add(this.cbCustomer);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Text = "HyundaiMan";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.TopMost = true;
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox cbCustomer;
+        private System.Windows.Forms.ComboBox cbType;
+        private System.Windows.Forms.TextBox tbInput;
+        private System.Windows.Forms.ListView lvResult;
+        private System.Windows.Forms.Button bSearch;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
     }
 }
 
