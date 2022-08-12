ef4384fce50154f652ab8f3181bf89c71855761c
ssw900528 <ssw900528@gmail.com>
2022-08-12 15:00:21 +0900

    1st complete regexv4

--- RegexerV4/RegexerV4/Form1.Designer.cs
+++ RegexerV4/RegexerV4/Form1.Designer.cs
@@ -32,16 +32,36 @@ namespace RegexerV4
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.tbMain = new System.Windows.Forms.TabControl();
             this.tpRegex = new System.Windows.Forms.TabPage();
-            this.tbTxt = new System.Windows.Forms.TabPage();
-            this.tbXml = new System.Windows.Forms.TabPage();
+            this.btRegexGo = new System.Windows.Forms.Button();
+            this.tbRegexOutput = new System.Windows.Forms.TextBox();
+            this.tbRegexInput = new System.Windows.Forms.TextBox();
+            this.cbRegexFormula = new System.Windows.Forms.ComboBox();
+            this.tpTxt = new System.Windows.Forms.TabPage();
+            this.tbTxtSeq = new System.Windows.Forms.TextBox();
+            this.tbTxtStarts = new System.Windows.Forms.TextBox();
+            this.btTxtCheck = new System.Windows.Forms.Button();
+            this.tbTxtContent = new System.Windows.Forms.TextBox();
+            this.dgTxt = new System.Windows.Forms.DataGridView();
+            this.tpXml = new System.Windows.Forms.TabPage();
+            this.tbXmlDirectory = new System.Windows.Forms.TextBox();
+            this.btXmlCheck = new System.Windows.Forms.Button();
+            this.tbXmlContent = new System.Windows.Forms.TextBox();
+            this.dgXml = new System.Windows.Forms.DataGridView();
+            this.btRegexLtoR = new System.Windows.Forms.Button();
             this.tbMain.SuspendLayout();
+            this.tpRegex.SuspendLayout();
+            this.tpTxt.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgTxt)).BeginInit();
+            this.tpXml.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgXml)).BeginInit();
             this.SuspendLayout();
             // 
             // tbMain
             // 
+            this.tbMain.AllowDrop = true;
             this.tbMain.Controls.Add(this.tpRegex);
-            this.tbMain.Controls.Add(this.tbTxt);
-            this.tbMain.Controls.Add(this.tbXml);
+            this.tbMain.Controls.Add(this.tpTxt);
+            this.tbMain.Controls.Add(this.tpXml);
             this.tbMain.Location = new System.Drawing.Point(12, 12);
             this.tbMain.Name = "tbMain";
             this.tbMain.SelectedIndex = 0;
@@ -50,6 +70,11 @@ namespace RegexerV4
             // 
             // tpRegex
             // 
+            this.tpRegex.Controls.Add(this.btRegexLtoR);
+            this.tpRegex.Controls.Add(this.btRegexGo);
+            this.tpRegex.Controls.Add(this.tbRegexOutput);
+            this.tpRegex.Controls.Add(this.tbRegexInput);
+            this.tpRegex.Controls.Add(this.cbRegexFormula);
             this.tpRegex.Location = new System.Drawing.Point(4, 22);
             this.tpRegex.Name = "tpRegex";
             this.tpRegex.Padding = new System.Windows.Forms.Padding(3);
@@ -58,35 +83,189 @@ namespace RegexerV4
             this.tpRegex.Text = "tpRegex";
             this.tpRegex.UseVisualStyleBackColor = true;
             // 
-            // tbTxt
+            // btRegexGo
             // 
-            this.tbTxt.Location = new System.Drawing.Point(4, 22);
-            this.tbTxt.Name = "tbTxt";
-            this.tbTxt.Padding = new System.Windows.Forms.Padding(3);
-            this.tbTxt.Size = new System.Drawing.Size(1302, 600);
-            this.tbTxt.TabIndex = 1;
-            this.tbTxt.Text = "tbTxt";
-            this.tbTxt.UseVisualStyleBackColor = true;
+            this.btRegexGo.Location = new System.Drawing.Point(1193, 6);
+            this.btRegexGo.Name = "btRegexGo";
+            this.btRegexGo.Size = new System.Drawing.Size(103, 23);
+            this.btRegexGo.TabIndex = 3;
+            this.btRegexGo.Text = "btRegexGo";
+            this.btRegexGo.UseVisualStyleBackColor = true;
+            this.btRegexGo.Click += new System.EventHandler(this.btRegexGo_Click);
             // 
-            // tbXml
+            // tbRegexOutput
             // 
-            this.tbXml.Location = new System.Drawing.Point(4, 22);
-            this.tbXml.Name = "tbXml";
-            this.tbXml.Size = new System.Drawing.Size(1302, 600);
-            this.tbXml.TabIndex = 2;
-            this.tbXml.Text = "tbXml";
-            this.tbXml.UseVisualStyleBackColor = true;
+            this.tbRegexOutput.Location = new System.Drawing.Point(637, 32);
+            this.tbRegexOutput.Multiline = true;
+            this.tbRegexOutput.Name = "tbRegexOutput";
+            this.tbRegexOutput.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.tbRegexOutput.Size = new System.Drawing.Size(659, 562);
+            this.tbRegexOutput.TabIndex = 2;
+            this.tbRegexOutput.WordWrap = false;
+            // 
+            // tbRegexInput
+            // 
+            this.tbRegexInput.Location = new System.Drawing.Point(6, 32);
+            this.tbRegexInput.Multiline = true;
+            this.tbRegexInput.Name = "tbRegexInput";
+            this.tbRegexInput.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.tbRegexInput.Size = new System.Drawing.Size(625, 562);
+            this.tbRegexInput.TabIndex = 1;
+            this.tbRegexInput.WordWrap = false;
+            // 
+            // cbRegexFormula
+            // 
+            this.cbRegexFormula.FormattingEnabled = true;
+            this.cbRegexFormula.Location = new System.Drawing.Point(6, 6);
+            this.cbRegexFormula.Name = "cbRegexFormula";
+            this.cbRegexFormula.Size = new System.Drawing.Size(1073, 20);
+            this.cbRegexFormula.TabIndex = 0;
+            // 
+            // tpTxt
+            // 
+            this.tpTxt.AllowDrop = true;
+            this.tpTxt.Controls.Add(this.tbTxtSeq);
+            this.tpTxt.Controls.Add(this.tbTxtStarts);
+            this.tpTxt.Controls.Add(this.btTxtCheck);
+            this.tpTxt.Controls.Add(this.tbTxtContent);
+            this.tpTxt.Controls.Add(this.dgTxt);
+            this.tpTxt.Location = new System.Drawing.Point(4, 22);
+            this.tpTxt.Name = "tpTxt";
+            this.tpTxt.Padding = new System.Windows.Forms.Padding(3);
+            this.tpTxt.Size = new System.Drawing.Size(1302, 600);
+            this.tpTxt.TabIndex = 1;
+            this.tpTxt.Text = "tpTxt";
+            this.tpTxt.UseVisualStyleBackColor = true;
+            // 
+            // tbTxtSeq
+            // 
+            this.tbTxtSeq.Location = new System.Drawing.Point(6, 371);
+            this.tbTxtSeq.Name = "tbTxtSeq";
+            this.tbTxtSeq.Size = new System.Drawing.Size(1290, 21);
+            this.tbTxtSeq.TabIndex = 5;
+            // 
+            // tbTxtStarts
+            // 
+            this.tbTxtStarts.Location = new System.Drawing.Point(6, 398);
+            this.tbTxtStarts.Name = "tbTxtStarts";
+            this.tbTxtStarts.Size = new System.Drawing.Size(1290, 21);
+            this.tbTxtStarts.TabIndex = 4;
+            // 
+            // btTxtCheck
+            // 
+            this.btTxtCheck.Location = new System.Drawing.Point(6, 571);
+            this.btTxtCheck.Name = "btTxtCheck";
+            this.btTxtCheck.Size = new System.Drawing.Size(1290, 23);
+            this.btTxtCheck.TabIndex = 2;
+            this.btTxtCheck.Text = "btTxtCheck";
+            this.btTxtCheck.UseVisualStyleBackColor = true;
+            this.btTxtCheck.Click += new System.EventHandler(this.btTxtCheck_Click);
+            // 
+            // tbTxtContent
+            // 
+            this.tbTxtContent.AllowDrop = true;
+            this.tbTxtContent.Location = new System.Drawing.Point(6, 425);
+            this.tbTxtContent.Multiline = true;
+            this.tbTxtContent.Name = "tbTxtContent";
+            this.tbTxtContent.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.tbTxtContent.Size = new System.Drawing.Size(1290, 140);
+            this.tbTxtContent.TabIndex = 1;
+            this.tbTxtContent.WordWrap = false;
+            // 
+            // dgTxt
+            // 
+            this.dgTxt.AllowUserToAddRows = false;
+            this.dgTxt.AllowUserToDeleteRows = false;
+            this.dgTxt.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgTxt.Location = new System.Drawing.Point(6, 6);
+            this.dgTxt.Name = "dgTxt";
+            this.dgTxt.ReadOnly = true;
+            this.dgTxt.RowTemplate.Height = 23;
+            this.dgTxt.Size = new System.Drawing.Size(1290, 359);
+            this.dgTxt.TabIndex = 0;
+            // 
+            // tpXml
+            // 
+            this.tpXml.Controls.Add(this.tbXmlDirectory);
+            this.tpXml.Controls.Add(this.btXmlCheck);
+            this.tpXml.Controls.Add(this.tbXmlContent);
+            this.tpXml.Controls.Add(this.dgXml);
+            this.tpXml.Location = new System.Drawing.Point(4, 22);
+            this.tpXml.Name = "tpXml";
+            this.tpXml.Size = new System.Drawing.Size(1302, 600);
+            this.tpXml.TabIndex = 2;
+            this.tpXml.Text = "tpXml";
+            this.tpXml.UseVisualStyleBackColor = true;
+            // 
+            // tbXmlDirectory
+            // 
+            this.tbXmlDirectory.Location = new System.Drawing.Point(6, 398);
+            this.tbXmlDirectory.Name = "tbXmlDirectory";
+            this.tbXmlDirectory.Size = new System.Drawing.Size(1290, 21);
+            this.tbXmlDirectory.TabIndex = 3;
+            // 
+            // btXmlCheck
+            // 
+            this.btXmlCheck.Location = new System.Drawing.Point(6, 571);
+            this.btXmlCheck.Name = "btXmlCheck";
+            this.btXmlCheck.Size = new System.Drawing.Size(1290, 23);
+            this.btXmlCheck.TabIndex = 2;
+            this.btXmlCheck.Text = "btXmlCheck";
+            this.btXmlCheck.UseVisualStyleBackColor = true;
+            this.btXmlCheck.Click += new System.EventHandler(this.btXmlCheck_Click);
+            // 
+            // tbXmlContent
+            // 
+            this.tbXmlContent.Location = new System.Drawing.Point(6, 425);
+            this.tbXmlContent.Multiline = true;
+            this.tbXmlContent.Name = "tbXmlContent";
+            this.tbXmlContent.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.tbXmlContent.Size = new System.Drawing.Size(1290, 140);
+            this.tbXmlContent.TabIndex = 1;
+            this.tbXmlContent.WordWrap = false;
+            // 
+            // dgXml
+            // 
+            this.dgXml.AllowUserToAddRows = false;
+            this.dgXml.AllowUserToDeleteRows = false;
+            this.dgXml.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgXml.Location = new System.Drawing.Point(6, 6);
+            this.dgXml.Name = "dgXml";
+            this.dgXml.ReadOnly = true;
+            this.dgXml.RowTemplate.Height = 23;
+            this.dgXml.Size = new System.Drawing.Size(1290, 386);
+            this.dgXml.TabIndex = 0;
+            // 
+            // btRegexLtoR
+            // 
+            this.btRegexLtoR.Location = new System.Drawing.Point(1085, 6);
+            this.btRegexLtoR.Name = "btRegexLtoR";
+            this.btRegexLtoR.Size = new System.Drawing.Size(102, 23);
+            this.btRegexLtoR.TabIndex = 4;
+            this.btRegexLtoR.Text = "btRegexLtoR";
+            this.btRegexLtoR.UseVisualStyleBackColor = true;
+            this.btRegexLtoR.Click += new System.EventHandler(this.btRegexLtoR_Click);
             // 
             // Form1
             // 
+            this.AllowDrop = true;
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1334, 650);
             this.Controls.Add(this.tbMain);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "RegexerV4";
             this.tbMain.ResumeLayout(false);
+            this.tpRegex.ResumeLayout(false);
+            this.tpRegex.PerformLayout();
+            this.tpTxt.ResumeLayout(false);
+            this.tpTxt.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgTxt)).EndInit();
+            this.tpXml.ResumeLayout(false);
+            this.tpXml.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgXml)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -95,8 +274,22 @@ namespace RegexerV4
 
         private System.Windows.Forms.TabControl tbMain;
         private System.Windows.Forms.TabPage tpRegex;
-        private System.Windows.Forms.TabPage tbTxt;
-        private System.Windows.Forms.TabPage tbXml;
+        private System.Windows.Forms.TabPage tpTxt;
+        private System.Windows.Forms.TabPage tpXml;
+        private System.Windows.Forms.ComboBox cbRegexFormula;
+        private System.Windows.Forms.TextBox tbRegexOutput;
+        private System.Windows.Forms.TextBox tbRegexInput;
+        private System.Windows.Forms.Button btRegexGo;
+        private System.Windows.Forms.DataGridView dgTxt;
+        private System.Windows.Forms.TextBox tbTxtContent;
+        private System.Windows.Forms.Button btTxtCheck;
+        private System.Windows.Forms.Button btXmlCheck;
+        private System.Windows.Forms.TextBox tbXmlContent;
+        private System.Windows.Forms.DataGridView dgXml;
+        private System.Windows.Forms.TextBox tbXmlDirectory;
+        private System.Windows.Forms.TextBox tbTxtStarts;
+        private System.Windows.Forms.TextBox tbTxtSeq;
+        private System.Windows.Forms.Button btRegexLtoR;
     }
 }
 
--- RegexerV4/RegexerV4/Form1.cs
+++ RegexerV4/RegexerV4/Form1.cs
@@ -3,18 +3,264 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace RegexerV4
 {
     public partial class Form1 : Form
     {
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool AllocConsole();
+
+        [DllImport("user32.dll", EntryPoint = "SetWindowPos")]
+        public static extern IntPtr SetWindowPos(IntPtr hWnd, int hWndInsertAfter, int x, int Y, int cx, int cy, int wFlags);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern IntPtr GetConsoleWindow();
+
+        public static readonly List<string> Templates = new List<string>();
+
         public Form1()
         {
             InitializeComponent();
+            AllocConsole();
+            SetWindowPos(GetConsoleWindow(), 0, -3000, 0, 0, 0, 0x0001);
+
+            Templates.AddRange(File.ReadAllText("Templates.txt").Replace("\r", "")
+                .Split(new string[] { "\n" }, StringSplitOptions.RemoveEmptyEntries));
+
+            cbRegexFormula.Items.AddRange(Templates.ToArray());
+            if (cbRegexFormula.Items.Count > 0)
+            {
+                cbRegexFormula.SelectedIndex = 0;
+            }
+        }
+
+        private string RegexBuild(string template, string content)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            string[] contentLines = content.Replace("\r", "").Replace("\t", "")
+                .Split(new string[] { "\n" }, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (string s in contentLines)
+            {
+                string target = template;
+                string[] contents = s.Split(new string[] { " " }
+                , StringSplitOptions.RemoveEmptyEntries);
+                for (int i = 0; i < contents.Length; i++)
+                {
+                    target = target.Replace("{" + i + "}", contents[i]);
+                }
+                sb.Append(target + Environment.NewLine);
+            }
+
+            return sb.ToString();
+        }
+
+        private string RegexLtoR(string content)
+        {
+            return content.Replace("\r", "").Replace("\n", " ");
+        }
+
+        private DataTable TxtToGrid(string content, string starts, string seq)
+        {
+            DataTable result = new DataTable();
+
+            string[] seqItems = seq.Split(new string[] { " " }
+            , StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (string s in seqItems)
+            {
+                string[] se = s.Split(new string[] { "\t" }
+                , StringSplitOptions.RemoveEmptyEntries);
+
+                result.Columns.Add(se[0] + "-" + se[1]);
+            }
+
+            string[] contentLines = content.Replace("\r", "").Split(new string[] { "\n" }
+            , StringSplitOptions.RemoveEmptyEntries);
+
+            int[] empty = new int[result.Columns.Count];
+            int x = 0;
+
+            foreach (string contentLine in contentLines)
+            {
+                if (contentLine.StartsWith(starts))
+                {
+                    DataRow row = result.NewRow();
+                    x = 0;
+                    foreach (string seqItem in seqItems)
+                    {
+                        string[] se = seqItem.Split(new string[] { "\t" }
+                        , StringSplitOptions.RemoveEmptyEntries);
+
+                        int s = int.Parse(se[0]), e = int.Parse(se[1]);
+                        string columnName = s + "-" + e;
+                        string itemStr = contentLine.Substring(s - 1, e - s + 1).Trim();
+                        if (itemStr.Length > 0)
+                        {
+                            row[columnName] = itemStr;
+                            empty[x] = 1;
+                        }
+                        x++;
+                    }
+                    result.Rows.Add(row);
+                }
+            }
+            Console.WriteLine(empty.Length);
+            Console.WriteLine(result.Columns.Count);
+
+            List<string> columnNames = new List<string>();
+
+            for (int i = 0; i < empty.Length; i++)
+            {
+                if (empty[i] == 0)
+                {
+                    columnNames.Add(result.Columns[i].ColumnName);
+                }
+            }
+
+            foreach (string columnName in columnNames)
+            {
+                result.Columns.Remove(columnName);
+            }
+
+            return result;
+        }
+
+        private DataTable TxtToGridSpace(string content, string starts)
+        {
+            DataTable result = new DataTable();
+
+            string[] contentLines = content.Replace("\r", "").Split(new string[] { "\n" }
+                , StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (string contentLine in contentLines)
+            {
+                List<KeyValuePair<int, string>> items = new List<KeyValuePair<int, string>>();
+                if (contentLine.StartsWith(starts))
+                {
+                    char[] arr = contentLine.ToCharArray();
+                    int p = -1, tp = 0, n = arr.Length, x = 0;
+                    StringBuilder sb = new StringBuilder();
+
+                    while (++p < n)
+                    {
+                        if (arr[p] == ' ')
+                        {
+                            if (x == 1)
+                            {
+                                items.Add(new KeyValuePair<int, string>(tp, sb.ToString()));
+                                sb.Clear();
+                            }
+                            x++;
+                        }
+                        else
+                        {
+                            if (x > 1)
+                            {
+                                tp = p;
+                            }
+                            x = 0;
+                            sb.Append(arr[p]);
+                        }
+                    }
+
+                    DataRow row = result.NewRow();
+                    foreach (KeyValuePair<int, string> kv in items)
+                    {
+                        if (!result.Columns.Contains(kv.Key + ""))
+                        {
+                            result.Columns.Add(kv.Key + "");
+                        }
+
+                        row[kv.Key + ""] = kv.Value;
+                    }
+                    result.Rows.Add(row);
+                }
+            }
+
+            return result;
+        }
+
+        private DataTable XmlToGrid(string s, string directory)
+        {
+            DataTable result = new DataTable();
+            XmlDocument xml = new XmlDocument();
+            xml.LoadXml(s);
+            XmlNodeList rows = xml.SelectNodes(directory);
+
+            if (rows.Count > 0 && rows[0].ChildNodes.Count > 0)
+            {
+                foreach (XmlNode n in rows[0].ChildNodes)
+                {
+                    result.Columns.Add(n.Name);
+                }
+
+                foreach (XmlNode n in rows)
+                {
+                    DataRow row = result.NewRow();
+                    foreach (XmlNode nn in n.ChildNodes)
+                    {
+                        if (!result.Columns.Contains(nn.Name))
+                        {
+                            break;
+                        }
+                        row[nn.Name] = nn.InnerText;
+                    }
+                    result.Rows.Add(row);
+                }
+            }
+
+            return result;
+        }
+
+        private void btRegexGo_Click(object sender, EventArgs e)
+        {
+            tbRegexOutput.Text = RegexBuild(cbRegexFormula.Text, tbRegexInput.Text);
+        }
+
+        private void btRegexLtoR_Click(object sender, EventArgs e)
+        {
+            tbRegexOutput.Text = RegexLtoR(tbRegexInput.Text);
+        }
+
+        private void btTxtCheck_Click(object sender, EventArgs e)
+        {
+            string text = tbTxtContent.Text;
+            string starts = tbTxtStarts.Text;
+            string seq = tbTxtSeq.Text;
+
+            dgTxt.Rows.Clear();
+            dgTxt.Columns.Clear();
+
+            if (seq == "")
+            {
+                dgTxt.DataSource = TxtToGridSpace(text, starts);
+            }
+            else
+            {
+                dgTxt.DataSource = TxtToGrid(text, starts, seq);
+            }
+        }
+
+        private void btXmlCheck_Click(object sender, EventArgs e)
+        {
+            string text = tbXmlContent.Text;
+            string directory = tbXmlDirectory.Text;
+
+            dgXml.Rows.Clear();
+            dgXml.Columns.Clear();
+
+            dgXml.DataSource = XmlToGrid(text, directory);
         }
     }
 }
